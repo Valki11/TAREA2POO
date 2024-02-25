d043d0e31b15c045f33490042fa73c46de3fb26c
Valki11 <102394314+Valki11@users.noreply.github.com>
2024-02-24 21:56:53 -0600

    Add files via upload

---
+++ If5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+main(){
+char lapiz,lapicero;
+cout<<"Trae Lapiz:";
+cin>>lapiz;
+cout<<"Trae lapicero:";
+cin>>lapicero;
+
+	if (lapiz=='s' || lapicero=='s' ){
+			cout<<lapiz<<endl;
+		cout<<"puedes entrar"<<endl;
+	}else{
+			cout<<"No puede Entrar"<<endl;
+		}
+	
+ 	
+system("pause");	
+}
---
+++ If6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main(){
+string pais="";	
+cout<<"Ingrese el Pais porfavor:";
+cin>>pais;
+
+	if (pais=="Guatemala"){
+		cout<<"El pais de la eterna primavera "<<pais<<endl;
+	}else{
+		cout<<"Tu eres de "<<pais<<endl;
+	}
+ 	
+system("pause");	
+}
