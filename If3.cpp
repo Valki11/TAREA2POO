c5fbf4391a6f06c458a43756eba6742575876273
Valki11 <102394314+Valki11@users.noreply.github.com>
2024-02-24 20:35:19 -0600

    Add files via upload

---
+++ If3.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+main(){
+	
+int num1=0,num2=0,num3=0,promedio=0;
+cout<<"Ingrese Nota 1 porfavor :  ";
+cin>>num1;
+cout<<"Ingrese Nota 2 porfavor :  ";
+cin>>num2;
+cout<<"Ingrese Nota 3 porfavor :  ";
+cin>>num3;
+promedio= (num1+num2+num3)/3;
+cout<<promedio<<endl;
+
+
+system("pause");
+
+}
